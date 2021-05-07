16bb0f1ffe2ee57503de5fc85633bf46373b194f
agiambro <giambrone@accuratebiz.com>
2021-05-06 19:15:55 -0500

    refactor and solve, wip

---
+++ app/lib/suduko.rb
@@ -0,0 +1,18 @@
+class GameBoard
+  VALID_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  def initialize *board
+    @board = board
+  end
+
+  def next_moves
+    row_1 = @board[0..8]
+    non_blanks = row_1.reject!{|i| i == 0}
+    possible_moves = VALID_NUMBERS - non_blanks
+    moves = possible_moves.each_with_object([]) do |i, accum|
+      board_copy = @board.clone
+      board_copy[0] = i
+      accum << GameBoard.new(*board_copy)
+    end[0...-1]
+  end
+end
